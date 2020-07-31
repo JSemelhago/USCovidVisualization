082fac8f1cb3d0f1799fa0848c290fbaca5689a5
Justin Semelhago <justin.semelhago@mail.utoronto.ca>
2020-07-31 18:44:45 -0400

    Edit GeoJSON
    Removed Guam

--- scripts/USCovidQGIS.py
+++ scripts/USCovidQGIS.py
@@ -1,11 +1,11 @@
 import os
 from qgis.core import QgsVectorLayer
 
-uscounties = 'uscounties_noterritories.geojson'
 
-county_layer = QgsVectorLayer(uscounties, 'County Layer', 'ogr')
+uscounties = '../data/uscounties_noterr.geojson'
 
-if not county_layer.isValid():
-    print('Loading the layer failed, please try again!')
-else:
-    QgsProject.instance().addMapLayer(county_layer)
+county_layer = iface.addVectorLayer(uscounties, 'County Layer', 'ogr')
+
+if not county_layer:
+    print('There was an error in loading the layer!')
+    
