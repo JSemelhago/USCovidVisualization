db46c2ce903cb130e597d58e07f52f9d6d8457fc
Justin Semelhago <justin.semelhago@mail.utoronto.ca>
2020-08-01 15:33:22 -0400

    Joined attributes in QGIS
    Added join attribute code, deleted unnecessary geojson file

--- scripts/USCovidQGIS.py
+++ scripts/USCovidQGIS.py
@@ -1,6 +1,7 @@
 import os
 import glob
 
+
 print('The current working directory is', os.getcwd())
 
 script_location = glob.glob('C:\\**\\USCovidVisualization\\scripts', recursive=True)[0]
@@ -14,4 +15,18 @@ county_layer = iface.addVectorLayer(uscounties, 'County Layer', 'ogr')
 
 if not county_layer:
     print('There was an error in loading the layer!')
-    
+
+data_location = glob.glob('../output/Processed*')[0]
+csv_layer = iface.addVectorLayer(data_location, 'Covid Data Layer', 'ogr')   
+
+geo_field = 'GEO_ID'
+csv_field = 'county_fips'
+
+joinObject.setJoinFieldName(csv_field)
+joinObject.setTargetFieldName(geo_field)
+joinObject.setJoinLayerId(csv_layer.id())
+
+joinObject.setUsingMemoryCache(True)
+joinObject.setJoinLayer(csv_layer)
+county_layer.addJoin(joinObject)
+
