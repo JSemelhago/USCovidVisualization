f39db8ff9edf676fce040e7d00d329dd9207de56
Justin Semelhago <justin.semelhago@mail.utoronto.ca>
2020-07-31 20:03:35 -0400

    Revert "Fixed working directory"
    This reverts commit 7b83a7b0eb1b0d1a7913f8191734dbe49b2d4e40.

--- scripts/USCovidQGIS.py
+++ scripts/USCovidQGIS.py
@@ -3,8 +3,8 @@ import glob
 
 print('The current working directory is', os.getcwd())
 
-
-print(os.path.realpath('USCovidQGIS.py'))
+script_location = glob.glob('C:\\**\\USCovidVisualization\\scripts', recursive=True)[0]
+os.chdir(script_location)
 
 print('The current working directory has now moved to this script location at', os.getcwd())
 
