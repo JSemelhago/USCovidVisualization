9f9e75f359ab27e6fbf4821dfe34217274aa7b6f
Justin Semelhago <justin.semelhago@mail.utoronto.ca>
2020-08-01 23:54:09 -0400

    Generate choropleth map
    General choropleth map can now be generated

--- scripts/USCovidQGIS.py
+++ scripts/USCovidQGIS.py
@@ -1,6 +1,9 @@
 import os
 import glob
-
+import re
+from pylab import cm
+import matplotlib as mpl
+import numpy as np
 
 print('The current working directory is', os.getcwd())
 
@@ -31,3 +34,46 @@ joinObject.setUsingMemoryCache(True)
 joinObject.setJoinLayer(csv_layer)
 county_layer.addJoin(joinObject)
 
+colour_list = []
+
+#INTERVAL NUMBER
+interval = 10
+
+
+
+
+#To get real number of spaces
+interval+=1
+
+cmap = cm.get_cmap('viridis', interval)
+
+#Generate Viridis colours
+for i in range(cmap.N):
+    #Only get rgb of rgba
+    rgb = cmap(i)[:3]
+    #Convert to hex
+    colour_list.append(mpl.colors.rgb2hex(rgb))
+
+field_names = [field.name() for field in county_layer.fields()]
+regex = re.compile(".*cases_per_100k")
+re_list = list(filter(regex.match, field_names))
+field_name = re_list[0]
+
+def apply_graduation(colour_list, interval):
+    range_list = []
+    idx = csv_layer.fields().indexFromName('cases_per_100k')
+    interval_list = np.linspace(csv_layer.minimumValue(idx), csv_layer.maximumValue(idx), interval)
+    for i in range(interval-1):
+        symbol = QgsSymbol.defaultSymbol(county_layer.geometryType())
+        symbol.setColor(QColor(colour_list[i]))
+        single_range = QgsRendererRange(interval_list[i], interval_list[i+1], symbol, 'Group '+str(i+1))
+        range_list.append(single_range)
+    
+    grad_renderer = QgsGraduatedSymbolRenderer(field_name, range_list)
+    grad_renderer.setMode(QgsGraduatedSymbolRenderer.Custom)
+    
+    county_layer.setRenderer(grad_renderer)
+    
+    print('Choropleth map has been created')
+    
+apply_graduation(colour_list, interval)
